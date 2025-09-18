ecde77cf832fc76c60988793035c71a3f377c4ae
Gazi Ehsan <153659358+GagiBik@users.noreply.github.com>
2025-09-18 16:45:53 -0600

    Add files via upload
    6 speed shifter Firmware using Mechadunio

---
+++ Mechaduino_6SShifter/Controller.cpp
@@ -0,0 +1,66 @@
+//Contains TC5 Controller definition
+//The main control loop is executed by the TC5 timer interrupt:
+
+#include <SPI.h>
+#include "State.h"
+#include "Utils.h"
+#include "Parameters.h"
+
+
+void TC5_Handler() {                              // gets called with FPID frequency
+  
+  static int print_counter = 0;                   //this is used by step response
+
+  if (TC5->COUNT16.INTFLAG.bit.OVF == 1) {        // A counter overflow caused the interrupt
+
+    TEST1_HIGH();  //digitalWrite(3, HIGH);       //Fast Write to Digital 3 for debugging
+
+    y = lookup[readEncoder()];                    //read encoder and lookup corrected angle in calibration lookup table
+   
+    if ((y - y_1) < -180.0) wrap_count += 1;      //Check if we've rotated more than a full revolution (have we "wrapped" around from 359 degrees to 0 or ffrom 0 to 359?)
+    else if ((y - y_1) > 180.0) wrap_count -= 1;
+
+    yw = (y + (360.0 * wrap_count));              //yw is the wrapped angle (can exceed one revolution)          
+    e = (r - yw);
+    
+    DTerm = pLPFa*DTerm -  pLPFb*pKd*(yw-yw_1);   // Integral term is not used/usefull.
+    u = (pKp * e) + DTerm; 
+
+    y_1 = y;  //copy current value of y to previous value (y_1) for next control cycle before PA angle added
+
+    if (u > 0){       //Depending on direction we want to apply torque, add or subtract a phase angle of PA for max effective torque.  
+                      //PA should be equal to one full step angle: if the excitation angle is the same as the current position, we would not move!  
+                      //You can experiment with "Phase Advance" by increasing PA when operating at high speeds
+      y += PA;        //update phase excitation angle
+      if (u > uMAX) u = uMAX;       //limit control effort saturation limits max current command
+    }
+    else {
+      y -= PA;          //update phase excitation angle
+      if (u < -uMAX)    // limit control effort
+        u = -uMAX;      //saturation limits max current command
+    }
+
+    U = abs(u);       //
+
+    if (abs(e) < 360) ledPin_HIGH();    // turn on LED if error is less than 0.1
+    else ledPin_LOW();                  //digitalWrite(ledPin, LOW);
+
+    output(-y, round(U));    // update phase currents
+    
+    e_2 = e_1;    //these past values can be useful for more complex controllers/filters.  Uncomment as necessary    
+    e_1 = e;
+    u_2 = u_1;
+    u_1 = u;
+    yw_1 = yw;
+    
+    if (print_yw ==  true){       //for step resonse... still under development
+      print_counter += 1;  
+      if (print_counter >= 5){    // print position every 5th loop (every time is too much data for plotter and may slow down control loop
+        SerialUSB.println(int(yw*1024));    //*1024 allows us to print ints instead of floats... may be faster
+        print_counter = 0;
+      }
+    }
+    TC5->COUNT16.INTFLAG.bit.OVF = 1;    // writing a one clears the flag ovf flag
+    TEST1_LOW();            //for testing the control loop timing
+  }
+}
